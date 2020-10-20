7fdce463d9aba1f86ccacf47ad6733603ef273b6
Yannick Biheul <yannickbiheul@outlook.fr>
2020-10-20 17:16:54 +0200

    Ajout du bouton Burger

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 const accueil = document.querySelector('#accueil');
-let hauteurAccueil = getComputedStyle(accueil).height;
+let hauteurAccueil = Number.parseInt(getComputedStyle(accueil).height);
+const header = document.querySelector('header');
+const boutonBurger = document.querySelector(".boutonBurger");
 
 console.log(hauteurAccueil);
+
+window.onscroll = function() {
+    if (document.documentElement.scrollTop >= hauteurAccueil) {
+        header.style.background = "#000";
+        header.style.height = "80px";
+        boutonBurger.style.top = "20px";
+    } else {
+        header.style.background = "transparent";
+        header.style.height = "100px";
+        boutonBurger.style.top = "30px";
+    }
+}
